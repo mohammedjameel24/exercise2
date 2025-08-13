26cccc62130534c35f95c8061cc0d061380e1baa
Mohd Jamil <mohammedjameel2412@gmail.com>
2025-08-13 07:05:31 +0000

    cleanup

--- config.py
+++ config.py
@@ -4,12 +4,12 @@ import urllib
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
-    SECRET_KEY = os.getenv("SECRET_KEY", "1749a1aee826464b0aac8da8ee758df044af78b586320eedd72c9c2cb276c886")
+    SECRET_KEY = os.getenv("SECRET_KEY", "")
 
-    SQL_SERVER = os.getenv("SQL_SERVER", "exercise2server.database.windows.net")
-    SQL_DATABASE = os.getenv("SQL_DATABASE", "exercise2")
-    SQL_USER_NAME = os.getenv("SQL_USER_NAME", "exercise2")
-    SQL_PASSWORD = os.getenv("SQL_PASSWORD", "Mohdjamil$123")
+    SQL_SERVER = os.getenv("SQL_SERVER", "")
+    SQL_DATABASE = os.getenv("SQL_DATABASE", "")
+    SQL_USER_NAME = os.getenv("SQL_USER_NAME", "")
+    SQL_PASSWORD = os.getenv("SQL_PASSWORD", "")
 
     DRIVER = os.getenv("ODBC_DRIVER", "ODBC Driver 18 for SQL Server")
 
@@ -33,6 +33,6 @@ class Config:
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     # Azure storage
-    BLOB_ACCOUNT = os.getenv("BLOB_ACCOUNT", "exercise2storageaccount")
-    BLOB_STORAGE_KEY = os.getenv("BLOB_STORAGE_KEY", "KEY")
-    BLOB_CONTAINER = os.getenv("BLOB_CONTAINER", "images")
+    BLOB_ACCOUNT = os.getenv("BLOB_ACCOUNT", "")
+    BLOB_STORAGE_KEY = os.getenv("BLOB_STORAGE_KEY", "")
+    BLOB_CONTAINER = os.getenv("BLOB_CONTAINER", "")
